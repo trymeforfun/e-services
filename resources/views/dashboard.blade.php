397df97e607cfd7af7e80b6061635b5c8c5813de
Okky Tirta Kurniawan <okkytirtakurniawan@192.168.1.22>
2023-08-14 00:34:51 +0700

    templating with sb admin & add breeze

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@include('partials.navbar')
+@include('partials.sidebar')
+
+
+
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Dashboard</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+
+    </div>
+</main>
+
+@include('partials.footer')
